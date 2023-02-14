0d1f97e8ae528ad54f31527d6a4a704ebb7957b9
asuapretni <asuapretni@gmail.com>
2023-02-14 20:20:42 +0100

    Change variables, comments, list of data types

---
+++ comments.py
@@ -0,0 +1,11 @@
+# Single line:
+# I'm a single line comment
+
+name = 'Kristine Hudgens'  # TODO: Split into two variables
+
+"""
+Multi line comment
+Multi line comment
+Multi line comment
+Multi line comment
+"""
---
+++ list_of_data_types.py
@@ -0,0 +1,17 @@
+# Booleans
+# Numbers
+# Strings
+# Bytes and byte arrays
+# None
+# Lists
+# Tuples
+# Sets
+# Dictionaries
+
+meal_completed = True
+sub_total = 100
+tip = sub_total * 0.2
+# tip = sub_total * 1/5
+total = sub_total + tip
+receipt = "Your total is " + str(total)
+print(receipt)
